a440e8c4ab4054c37a287fa874aa12364b7b81b8
Ben Vissotzky and Ryan Loos <ryan@>
2016-08-17 15:51:56 -0700

    add check for punctuation at the end of words

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
+
 //<!-- Back End -->
 function textTest(string) {
   var numTest = /\d/;
   return numTest.test(string);
   }
+
+function if_punctuation(string) {
+  var punctuation;
+  var findPunk = /[.,;:!?]/g;
+  if (findPunk.test(string)) {
+    punctuation = string[string.length - 1];
+    return punctuation;
+  }
+}
+
 // if true move on. if false error message
 
 function qtest(string) {
@@ -14,20 +25,30 @@ function qtest(string) {
 function qword(string) {
   var match = /qu/.exec(string);
   var string = string.split('');
+  var punctuation = '';
+  if(if_punctuation(string)) {
+    punctuation = if_punctuation(string);
+    string.pop();
+  }
   var indexes = [];
   indexes.push(match.index);
   var array = string.slice(0,(match.index + 2));
   var newarray = string.slice(match.index + 2,string.length);
   newarray = newarray.concat(array);
-  var final = newarray.join('') + 'ay';
+  var final = newarray.join('') + 'ay' + punctuation;
   return final;
 }
 //word ends up here, gets ending
 
 function consonantTest(string) {
-  var vowelTest = /[aeiou]/gi;
   var consonantsArray = [];
   var stringArray = string.split('');
+  var punctuation = '';
+  if(if_punctuation(string)) {
+    punctuation = if_punctuation(string);
+    stringArray.pop();
+  }
+  var vowelTest = /[aeiou]/gi;
   for (var i = 0; i < stringArray.length; i++) {
     if (vowelTest.test(stringArray[i])) {
       break;
@@ -38,7 +59,7 @@ function consonantTest(string) {
     }
   }
   stringArray = stringArray.concat(consonantsArray);
-  var consonantTranslate = stringArray.join('') + 'ay';
+  var consonantTranslate = stringArray.join('') + 'ay' + punctuation;
   return consonantTranslate;
 }
 //word ends up here, gets ending
@@ -60,7 +81,7 @@ $(document).ready(function(){
     if (userAlert) {
       alert('please enter a sentence without numbers');
     } else if (sentence === ''){
-      alert('please enter a sentence') 
+      alert('please enter a sentence')
     } else {
       var userArray = sentence.split(" ");
       userArray.forEach(function(word) {
